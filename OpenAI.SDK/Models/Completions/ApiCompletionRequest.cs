4b86848f3b3a9554a634c3672780115868a7c4dc
Vyacheslav Benedichuk <vbenedichuk@gmail.com>
2023-02-11 13:48:05 +0400

    models cleanup

--- OpenAI.SDK/Models/Completions/ApiCompletionRequest.cs
+++ OpenAI.SDK/Models/Completions/ApiCompletionRequest.cs
@@ -1,26 +1,103 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace OpenAI.SDK.Models.Completions
 {
     public class ApiCompletionRequest
     {
+        /// <summary>
+        /// ID of the model to use. You can use the <see href="https://platform.openai.com/docs/api-reference/models/list">List models API</see> to see all of your available models, or see our <see href="https://platform.openai.com/docs/models/overview">Model overview</see> for descriptions of them.
+        /// </summary>
         [JsonPropertyName("model")]
         public string Model { get; set; }
+        /// <summary>
+        /// The prompt(s) to generate completions for, encoded as a string, array of strings, array of tokens, or array of token arrays.
+        /// Note that<|endoftext|> is the document separator that the model sees during training, 
+        /// so if a prompt is not specified the model will generate as if from the beginning of a new document.
+        /// </summary>
         [JsonPropertyName("prompt")]
         public string Prompt { get; set; }
+        /// <summary>
+        /// The suffix that comes after a completion of inserted text.
+        /// </summary>
+        [JsonPropertyName("suffix")]
+        public string Suffix { get; set; }
+        /// <summary>
+        /// The maximum number of <see href="https://platform.openai.com/tokenizer">tokens</see> to generate in the completion.
+        /// The token count of your prompt plus max_tokens cannot exceed the model's context length. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+        /// </summary>
         [JsonPropertyName("max_tokens")]
         public int? MaxTokens { get; set; }
+        /// <summary>
+        /// What sampling temperature to use, between 0 and 2. Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic.
+        /// We generally recommend altering this or top_p but not both.
+        /// </summary>
         [JsonPropertyName("temperature")]
-        public int Temperatue { get; set; }
+        public float Temperatue { get; set; }
+        /// <summary>
+        /// An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered.
+        /// We generally recommend altering this or temperature but not both.
+        /// </summary>
         [JsonPropertyName("top_p")]
-        public int? TopP { get; set; }
+        public float? TopP { get; set; }
+        /// <summary>
+        /// How many completions to generate for each prompt.
+        /// Note: Because this parameter generates many completions, it can quickly consume your token quota.Use carefully and ensure that you have reasonable settings for max_tokens and stop.
+        /// </summary>
         [JsonPropertyName("n")]
         public int? N { get; set; }
+        /// <summary>
+        /// Whether to stream back partial progress. If set, tokens will be sent as data-only <see href="https://developer.mozilla.org/en-US/docs/Web/API/Server-sent_events/Using_server-sent_events#Event_stream_format">server-sent events</see> as they become available, with the stream terminated by a data: [DONE] message.
+        /// </summary>
         [JsonPropertyName("stream")]
         public bool? Stream { get; set; }
+        /// <summary>
+        /// Include the log probabilities on the logprobs most likely tokens, as well the chosen tokens. For example, if logprobs is 5, the API will return a list of the 5 most likely tokens. The API will always return the logprob of the sampled token, so there may be up to logprobs+1 elements in the response.
+        /// The maximum value for logprobs is 5. If you need more than this, please contact us through our <see href="https://help.openai.com/">Help center</see> and describe your use case.
+        /// </summary>
         [JsonPropertyName("logprobe")]
         public int? Logprobe { get; set; }
+        /// <summary>
+        /// Echo back the prompt in addition to the completion
+        /// </summary>
+        [JsonPropertyName("echo")]
+        public bool? Echo { get; set; }
+        /// <summary>
+        /// Up to 4 sequences where the API will stop generating further tokens. The returned text will not contain the stop sequence.
+        /// </summary>
         [JsonPropertyName("stop")]
         public string Stop { get; set; }
+        /// <summary>
+        /// Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+        /// <see href="https://platform.openai.com/docs/api-reference/parameter-details">See more information about frequency and presence penalties.</see>
+        /// </summary>
+        [JsonPropertyName("presence_penalty")]
+        public float? PresencePenalty { get; set; }
+        /// <summary>
+        /// Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+        /// <see href="https://platform.openai.com/docs/api-reference/parameter-details">See more information about frequency and presence penalties.</see> 
+        /// </summary>
+        [JsonPropertyName("frequency_penalty")]
+        public float? FrequencyPenalty { get; set; }
+
+        /// <summary>
+        /// Generates best_of completions server-side and returns the "best" (the one with the highest log probability per token). Results cannot be streamed.
+        /// When used with n, best_of controls the number of candidate completions and n specifies how many to return – best_of must be greater than n.
+        /// Note: Because this parameter generates many completions, it can quickly consume your token quota.Use carefully and ensure that you have reasonable settings for max_tokens and stop.
+        /// </summary>
+        [JsonPropertyName("best_of")]
+        public int? BestOf { get; set; }
+        /// <summary>
+        /// Modify the likelihood of specified tokens appearing in the completion.
+        /// Accepts a json object that maps tokens(specified by their token ID in the GPT tokenizer) to an associated bias value from -100 to 100. You can use this tokenizer tool (which works for both GPT-2 and GPT-3) to convert text to token IDs. Mathematically, the bias is added to the logits generated by the model prior to sampling. The exact effect will vary per model, but values between -1 and 1 should decrease or increase likelihood of selection; values like -100 or 100 should result in a ban or exclusive selection of the relevant token.
+        /// As an example, you can pass { "50256": -100} to prevent the <|endoftext|> token from being generated.
+        /// </summary>
+        [JsonPropertyName("logit_bias")]
+        public Dictionary<string, float> LogitBias { get; set; }
+        /// <summary>
+        /// A unique identifier representing your end-user, which can help OpenAI to monitor and detect abuse. <see href="https://platform.openai.com/docs/guides/safety-best-practices/end-user-ids">Learn more</see>.
+        /// </summary>
+        [JsonPropertyName("user")]
+        public string User { get; set; }
     }
 }
--- OpenAI.SDK/Models/Edits/ApiCreateEditRequest.cs
+++ OpenAI.SDK/Models/Edits/ApiCreateEditRequest.cs
@@ -4,16 +4,37 @@ namespace OpenAI.SDK.Models.Edits
 {
     public class ApiCreateEditRequest
     {
+        /// <summary>
+        /// The ID of the model to use.
+        /// Can be either "text-davinci-edit-001" or "code-davinci-edit-001".
+        /// </summary>
         [JsonPropertyName("model")]
         public string Model { get; set; }
+        /// <summary>
+        /// The input text to use as a starting point for the edit.
+        /// </summary>
         [JsonPropertyName("input")]
         public string Input { get; set; }
+        /// <summary>
+        /// The instruction that tells the model how to edit the input.
+        /// </summary>
         [JsonPropertyName("instruction")]
         public string Instruction { get; set; }
+        /// <summary>
+        /// The sampling temperature to use, between 0 and 2.
+        /// Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic.
+        /// </summary>
         [JsonPropertyName("temperature")]
-        public int Temperatue { get; set; }
+        public float Temperatue { get; set; }
+        /// <summary>
+        /// The top_p value to use for nucleus sampling, where the model considers the results of the tokens with top_p probability mass.
+        /// So 0.1 means only the tokens comprising the top 10% probability mass are considered.
+        /// </summary>
         [JsonPropertyName("top_p")]
         public int? TopP { get; set; }
+        /// <summary>
+        /// The number of edits to generate for the input and instruction.
+        /// </summary>
         [JsonPropertyName("n")]
         public int? N { get; set; }
     }
